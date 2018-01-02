3de9603ed475894146ed47075d6472189bdfcfa2
lvwanyou <lvwanyou@163.com>
2018-01-02 23:30:47 +0800

    Initial commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿using System;
+
+namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pager3 = new WHC.Pager.WinControl.Pager();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // pager3
+            // 
+            this.pager3.CurrentPageIndex = 1;
+            this.pager3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pager3.Location = new System.Drawing.Point(1495, 220);
+            this.pager3.Margin = new System.Windows.Forms.Padding(4);
+            this.pager3.Name = "pager3";
+            this.pager3.RecordCount = 0;
+            this.pager3.Size = new System.Drawing.Size(863, 44);
+            this.pager3.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(455, 158);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(564, 158);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 29);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(673, 158);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 29);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(783, 158);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 29);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(905, 158);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 29);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(201, 171);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "当前0/0页";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(347, 158);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 29);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "<";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1032, 156);
+            this.button7.Margin = new System.Windows.Forms.Padding(4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 29);
+            this.button7.TabIndex = 12;
+            this.button7.Text = ">";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(737, 259);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 26);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "跳转";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(621, 259);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 16;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1495, 585);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pager3);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private WHC.Pager.WinControl.Pager pager3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    
+    public partial class Form1 : Form
+    {
+        private int CurrentPage = 1;
+        private int startPage = 1;
+        private int endPage =15;
+        private int range = 5;
+        public Form1()
+        {
+            InitializeComponent();
+            initPager();
+        }
+
+        //根据不同的endPage去初始化 button
+        public void initPager()
+        {
+            label1.Text = "当前" + startPage + "/" + endPage + "页";
+            if (endPage > 5)
+            {
+                button1.Text = "1";
+                button2.Text = "2";
+                button3.Text = "3";
+                button4.Text = "4";
+                button5.Text = "5";
+            }
+            else
+            {
+                for (int i = 0; i < endPage; i++)
+                {
+                    if(i==0)
+                    {
+                        button1.Text = "1";
+                    }
+                    if (i == 1)
+                    {
+                        button2.Text = "2";
+                    }
+                    if (i == 2)
+                    {
+                        button3.Text = "3";
+                    }
+                    if (i == 3)
+                    {
+                        button4.Text = "4";
+                    }
+                    if (i == 4)
+                    {
+                        button5.Text = "5";
+                    }
+                }
+            }
+            button1.BackColor = System.Drawing.Color.Red;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            changeStatus(button1);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            changeStatus(button2);
+        }
+  
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            changeStatus(button3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            changeStatus(button4);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            changeStatus(button5);
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            getButtonStatusByColor(button6);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            getButtonStatusByColor(button7);
+        }
+
+        public void getButtonStatusByColor(Button button )
+        {
+    
+            //首先得到颜色显示在哪个button中，即事件焦点显示
+            if (button1.BackColor == System.Drawing.Color.Red)
+            {
+                if (button.Name == "button6")
+                {
+                    //不做处理
+                }else if (button.Name == "button7")
+                {
+                    changeStatus(button2);
+                }
+            }else
+            if (button2.BackColor == System.Drawing.Color.Red)
+            {
+                if (button.Name == "button6")
+                {
+                    changeStatus(button1);
+                }
+                else if (button.Name == "button7")
+                {
+                    changeStatus(button3);
+                }
+            }else
+            if (button3.BackColor == System.Drawing.Color.Red)
+            {
+                if (button.Name == "button6")
+                {
+                    changeStatus(button2);
+                }
+                else if (button.Name == "button7")
+                {
+                    changeStatus(button4);
+                }
+            }else
+            if (button4.BackColor == System.Drawing.Color.Red)
+            {
+                if (button.Name == "button6")
+                {
+                    changeStatus(button3);
+                }
+                else if (button.Name == "button7")
+                {
+                    changeStatus(button5);
+                }
+            }else
+            if (button5.BackColor == System.Drawing.Color.Red)
+            {
+                if (button.Name == "button6")
+                {
+                    changeStatus(button4);
+                }
+                else if (button.Name == "button7")
+                {
+                    //不做处理
+                }
+            }
+
+        }
+       
+
+        //改变被点击按钮的颜色
+        public void changeStatus(Button button)
+        {
+            button1.BackColor = Control.DefaultBackColor;
+            button2.BackColor = Control.DefaultBackColor;
+            button3.BackColor = Control.DefaultBackColor;
+            button4.BackColor = Control.DefaultBackColor;
+            button5.BackColor = Control.DefaultBackColor;
+            CurrentPage=Convert.ToInt32(button.Text);       //获取当前页；被点击后页
+            label1.Text ="当前" + CurrentPage + "/" + endPage + "页";
+    
+            if (endPage > 5)
+            {
+                //1-10
+                if (System.Math.Abs(CurrentPage - startPage) >= 4 && System.Math.Abs(CurrentPage - endPage) >= 4)
+                {
+                    button1.Text = Convert.ToString(CurrentPage - 2);
+                    button2.Text = Convert.ToString(CurrentPage - 1);
+                    button3.Text = Convert.ToString(CurrentPage);
+                    button4.Text = Convert.ToString(CurrentPage + 1);
+                    button5.Text = Convert.ToString(CurrentPage + 2);
+                    button3.BackColor = System.Drawing.Color.Red;
+                }
+                else if (System.Math.Abs(CurrentPage - startPage) >= 4 && System.Math.Abs(CurrentPage - endPage) < 4)
+                {
+                    int currentButton = range - System.Math.Abs(CurrentPage - endPage);
+                    getCurrentButton(currentButton);
+                }
+                else
+                {
+                    getCurrentButton(CurrentPage);
+                }
+  /*             else if (System.Math.Abs(CurrentPage - startPage) < 4 && System.Math.Abs(CurrentPage - endPage) > 4)
+                {
+                  getCurrentButton(CurrentPage);              
+                }
+                else if (System.Math.Abs(CurrentPage - startPage) < 4 && System.Math.Abs(CurrentPage - endPage) <= 4)
+                {
+                 getCurrentButton(CurrentPage);                 
+                }
+   */
+            }
+            else   // 如果总页数小于五，就把对应的点击按钮变色
+            {
+                button.BackColor = System.Drawing.Color.Red;
+            }
+        }
+
+        //如果button 需居中，刷新5个button的状态
+        public Button getCurrentButton(int num)
+        {
+            button1.BackColor = Control.DefaultBackColor;
+            button2.BackColor = Control.DefaultBackColor;
+            button3.BackColor = Control.DefaultBackColor;
+            button4.BackColor = Control.DefaultBackColor;
+            button5.BackColor = Control.DefaultBackColor;
+            switch (num){
+            case 1:
+                    button1.Text = Convert.ToString(CurrentPage);
+                    button2.Text = Convert.ToString(CurrentPage + 1);
+                    button3.Text = Convert.ToString(CurrentPage+2);
+                    button4.Text = Convert.ToString(CurrentPage + 3);
+                    button5.Text = Convert.ToString(CurrentPage + 4);
+                    button1.BackColor = System.Drawing.Color.Red;
+                    return button1;
+                    break;
+            case 2:
+                   button1.Text = Convert.ToString(CurrentPage-1);
+                    button2.Text = Convert.ToString(CurrentPage );
+                    button3.Text = Convert.ToString(CurrentPage+1);
+                    button4.Text = Convert.ToString(CurrentPage + 2);
+                    button5.Text = Convert.ToString(CurrentPage + 3);
+                    button2.BackColor = System.Drawing.Color.Red;
+                    return button2;
+                    break;
+            case 3:
+                    button1.Text = Convert.ToString(CurrentPage-2);
+                    button2.Text = Convert.ToString(CurrentPage - 1);
+                    button3.Text = Convert.ToString(CurrentPage);
+                    button4.Text = Convert.ToString(CurrentPage +1);
+                    button5.Text = Convert.ToString(CurrentPage +2);
+                    button3.BackColor = System.Drawing.Color.Red;
+                    return button3;
+                    break;
+            case 4:
+                    button1.Text = Convert.ToString(CurrentPage - 3);
+                    button2.Text = Convert.ToString(CurrentPage - 2);
+                    button3.Text = Convert.ToString(CurrentPage-1);
+                    button4.Text = Convert.ToString(CurrentPage );
+                    button5.Text = Convert.ToString(CurrentPage + 1);
+                    button4.BackColor = System.Drawing.Color.Red;
+                    return button4;
+                    break;
+            case 5:
+                    button1.Text = Convert.ToString(CurrentPage -4);
+                    button2.Text = Convert.ToString(CurrentPage -3);
+                    button3.Text = Convert.ToString(CurrentPage -2);
+                    button4.Text = Convert.ToString(CurrentPage-1);
+                    button5.Text = Convert.ToString(CurrentPage);
+                    button5.BackColor = System.Drawing.Color.Red;
+                    return button5;
+                    break;
+            }
+            return null;
+        }
+
+  
+        private void button8_Click(object sender, EventArgs e)
+        {
+            CurrentPage = Convert.ToInt32(textBox2.Text);
+            if (CurrentPage < 5)
+            {
+                getCurrentButton(CurrentPage);
+            }else
+            {
+                //
+                Button temp = new Button();
+                temp.Text = Convert.ToString(CurrentPage);
+                changeStatus(temp);
+            }
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
